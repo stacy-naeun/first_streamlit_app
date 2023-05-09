0a33760e37681e5c0d8d120545d7f9228168a250
stacy-naeun <132968451+stacy-naeun@users.noreply.github.com>
2023-05-09 13:24:49 +0900

    Update streamlit_app.py
    test streamlit.write

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.write('Breakfast Menu
+                Omega 3 & Bluberry Oatmeal
+                Kale, Spinach & Rocket Smoothie
+                Hard-Boiled Free-Range Egg')
