6128d1c0af4bac4c4339ac17c787ee2638d6b19c
stacy-naeun <132968451+stacy-naeun@users.noreply.github.com>
2023-05-10 14:18:07 +0900

    Update streamlit_app.py
    modified fruityvice_response var

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,7 @@ fruit_choice = streamlit.text_input('What fruit would you like information about
 streamlit.write('The user entered ', fruit_choice)
 
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 # streamlit.text(fruityvice_response.json()) #just writes the data to the screen
 
 # write your own comment -what does the next line do? 
